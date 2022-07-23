12839a44f5e5715fc534889d0b0491c59e79e30d
RenBryant <renbee2bee@gmail.com>
2022-07-23 14:00:55 -0400

    added choiced

--- app.js
+++ app.js
@@ -1,1 +1,75 @@
 const { prompt } = require("inquirer");
+const table = require ("console.table")
+
+const {
+    searchDepartments,
+    searchRoles,
+    searchEmployees,
+    addDepartment,
+    addRole,
+    addEmployee,
+    updateEmployee,
+} = require ("./connection")
+
+async function questionPrompt() {
+const response = await prompt({
+    type: "list",
+    name: "choice",
+    message: "Which task?",
+    choices: [
+    {
+        name: "Search Through Departments",
+        value: "search_departments"
+    },
+    {
+        name: "Search Through Roles",
+        value: "search_roles"
+    },
+    {
+        name: "Search Through Employees",
+        value: "search_employees"
+    },
+    {
+        name: "Add 1 Department",
+        value: "add_department"
+    },
+    {
+        name: "Add 1 Role",
+        value: "add_role"
+    },
+    {
+        name: "Add 1 Employee",
+        value: "add_employee"
+    },
+    {
+        name: "Update an Employee",
+        value: "update_employee"
+    },
+    {
+        name: "quit",
+        value: "quit"
+    },
+    ]
+})
+
+    const choice = response.choice
+
+    switch (choice) {
+        case "search_departments":
+            return searchDepartments();
+        case "search_roles":
+            return searchRoles();
+        case "search_employees":
+                return searchEmployees();
+        case "add_department":
+                return addDepartment();
+        case "add_role":
+            return addRole();
+        case "add_employee":
+                return addEmployee();            
+        case "update_employee":
+            return updateEmployee();
+        case "quit":
+                return quit();
+    }
+};
--- db/db.sql
+++ db/db.sql
@@ -1,2 +1,3 @@
+-- Active: 1658583857031@@127.0.0.1@3306@employees_db
 CREATE DATABASE employees_db;
 USE employees_db;
